140fbc8af175fbdf2be683fcb49122de45969124
aftly <aftly@proton.me>
2025-10-07 22:59:41 +0100

    update exceptions and flag data

--- add_manually.py
+++ add_manually.py
@@ -18,6 +18,8 @@ flag_image_filenames_select_new_path = os.path.join(data_DIR, "flag_image_filena
 
 flag_of_is_the_exceptions_path = os.path.join(exceptions_DIR, "flag_of_is_the_exceptions.json")
 flag_of_is_the_exceptions_new_path = os.path.join(exceptions_DIR, "flag_of_is_the_exceptions_new.json")
+flag_of_official_is_the_exceptions_path = os.path.join(exceptions_DIR, "flag_of_official_is_the_exceptions.json")
+flag_of_official_is_the_exceptions_new_path = os.path.join(exceptions_DIR, "flag_of_official_is_the_exceptions_new.json")
 label_exceptions_path = os.path.join(exceptions_DIR, "label_exceptions.json")
 label_exceptions_new_path = os.path.join(exceptions_DIR, "label_exceptions_new.json")
 alias_exceptions_path = os.path.join(exceptions_DIR, "alias_exceptions.json")
@@ -48,11 +50,14 @@ with open(flags_dict_all_params_path, "r") as file:
 with open(flag_image_filenames_path, "r") as file:
     flag_image_filenames = json.load(file)   
     
-with open(flag_image_download_failed_path, "r") as file:
-    flag_image_download_failed = json.load(file)
+#with open(flag_image_download_failed_path, "r") as file:
+#    flag_image_download_failed = json.load(file)
     
 with open(flag_of_is_the_exceptions_path, "r") as file:
     flag_of_is_the_exceptions = json.load(file)
+    
+with open(flag_of_official_is_the_exceptions_path, "r") as file:
+    flag_of_official_is_the_exceptions = json.load(file)
     
 with open(label_exceptions_path, "r") as file:
     label_exceptions = json.load(file)
@@ -76,7 +81,6 @@ with open(svg_exceptions_preview_path, "r") as file:
     svg_exceptions_preview = json.load(file)
 
 
-
 #with open(flags_dict_all_params_new_path, "w") as file:
 #    json.dump(flags_dict, file, indent=4, sort_keys=True)
     
@@ -88,6 +92,9 @@ with open(svg_exceptions_preview_path, "r") as file:
 
 #with open(flag_of_is_the_exceptions_new_path, "w") as file:
 #    json.dump(flag_of_is_the_exceptions, file, indent=4, sort_keys=True)
+
+#with open(flag_of_official_is_the_exceptions_new_path, "w") as file:
+#    json.dump(flag_of_official_is_the_exceptions, file, indent=4, sort_keys=True)
 
 #with open(label_exceptions_new_path, "w") as file:
 #    json.dump(label_exceptions, file, indent=4, sort_keys=True)
